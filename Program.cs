d8130c5d6ab1fa5c14d6bc4151ce7b5b95b28df2
Christian Oliveira da Silva <pro2christian@icloud.com>
2023-07-19 21:46:41 -0300

    teste, push

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+public class App
+{   
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá, teste");
+        }
+    
+}
