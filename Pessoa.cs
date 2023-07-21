46e0d654f7a2b6b51757d2778345179a1a95a17e
Christian Oliveira da Silva <pro2christian@icloud.com>
2023-07-21 12:44:09 -0300

    add-app

--- Pessoa.cs
+++ Pessoa.cs
@@ -14,24 +14,54 @@ namespace ConsoleApp1
         //calculo
         public double calculo()
         {
-            return peso / (altura*altura);
+            return peso / (altura * altura);
         }
 
-        public string situacao(int imc)
+        public string situacao(double imc)
         {   //variavel
             string retorno;
 
             if (imc < 18.5)
             {
                 retorno = "Abaixo do peso";
-            } else if ()
+            }
+            else if (imc < 24.9)
             {
+                retorno = "Peso normal";
 
             }
+            else if (imc < 29.9)
+            {
+                retorno = "Excesso de peso";
+            }
+            else if (imc < 34.9)
+            {
+                retorno = "Obsidade classe I";
+            }
+            else if (imc < 39.9)
+            {
+                retorno = "Obsidade classe II";
 
+            }
+            else
+            {
+                retorno = "Obsidade classe III";
+            }
+            //retorno
+            return retorno;
         }
-
+        public void Dados()
+        {   //recebe calculo
+            double obterCaulculo = calculo();
+            //recebe situação
+            string obeterSituacao = situacao(obterCaulculo);
+                       
+        }
     }
-    
+        
 
 }
+
+    
+    
+
